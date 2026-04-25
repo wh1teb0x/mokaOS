06182f4657f09d6edcd6934e702d729efeab3176
Aki Takeuchi <akinori.take@gmail.com>
2026-04-26 02:14:14 +0900

    refactor pixel drawing into PixelWriter abstract class

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stddef.h>
 
 #include "frame_buffer_config.hpp"
 
@@ -8,30 +9,75 @@ struct PixelColor {
     uint8_t b;
 };
 
-int WritePixel(const FrameBufferConfig& config, int x, int y, const PixelColor& c) {
- const int pixel_position = config.pixels_per_scan_line * y + x;
- if (config.pixel_format == kPixelRGBResv8BitPerColor) {
-   uint32_t color = (c.r << 16) | (c.g << 8) | c.b;
-   *reinterpret_cast<uint32_t*>(config.frame_buffer + pixel_position * 4) = color;
- } else if (config.pixel_format == kPixelBGRResv8BitPerColor) {
-   uint32_t color = (c.b << 16) | (c.g << 8) | c.r;
-   *reinterpret_cast<uint32_t*>(config.frame_buffer + pixel_position * 4) = color;
- } else {
-   return -1; // Unsupported pixel format.
- }
-  return 0; // Success.
+class PixelWriter {
+  public:
+    PixelWriter(const FrameBufferConfig& config) : config_{config} { //constructor of PixelWriter
+    }
+    virtual ~PixelWriter() = default; // Virtual destructor to ensure proper cleanup of derived classes.
+    virtual void Write(int x, int y, const PixelColor& c) = 0; // 0 means pure virtual function, which must be implemented by derived classes, which means interface of PixelWriter is defined by this function.
+
+  protected:
+    uint8_t* PixelAt(int x, int y) {
+    return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
+    }
+  
+  private:
+    const FrameBufferConfig& config_;
+};
+
+class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+  public:
+    using PixelWriter::PixelWriter; // Inherit the constructor of the base class PixelWriter. C++11 feature that allows the derived class to use the constructor of the base class without having to redefine it.
+
+    virtual void Write(int x, int y, const PixelColor& c) override {
+      auto p = PixelAt(x, y);
+      p[0] = c.r;
+      p[1] = c.g;
+      p[2] = c.b;
+    }
+};
+
+class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+  public:
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor& c) override {
+      auto p = PixelAt(x, y);
+      p[0] = c.b;
+      p[1] = c.g;
+      p[2] = c.r;
+    }
+};
+
+
+void* operator new(size_t size, void* buf) {
+  return buf;
 }
 
+void operator delete(void* obj) noexcept {
+}
+
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter* pixel_writer;
+
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config){
-  // Draw a simple gradient on the screen using the provided frame buffer configuration.
-  for (uint32_t x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
-    for (uint32_t y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-      WritePixel(frame_buffer_config, x, y, {255, 255, 255});
-    };
-  };
+  switch (frame_buffer_config.pixel_format) {
+    case kPixelBGRResv8BitPerColor:
+      pixel_writer = new(pixel_writer_buf) BGRResv8BitPerColorPixelWriter(frame_buffer_config);
+      break;
+    case kPixelRGBResv8BitPerColor:
+      pixel_writer = new(pixel_writer_buf) RGBResv8BitPerColorPixelWriter(frame_buffer_config);
+      break;
+  }
+
+  for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
+    for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
+      pixel_writer->Write(x, y, {255, 255, 255});
+    }
+  }
   for (int x = 0; x < 200; ++x) {
     for (int y = 0; y < 100; ++y) {
-      WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
+      pixel_writer->Write(100 + x, 100 + y, {0, 255, 0});
     }
   }
   while (1) __asm__("hlt");
