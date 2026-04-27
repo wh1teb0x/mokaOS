a4ffb46bc96b90d16b8527cdaa4e91e485e4c564
Aki Takeuchi <akinori.take@gmail.com>
2026-04-27 17:14:24 +0900

    add printk using vsprintf

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,12 +1,12 @@
 #include <cstdio>
 #include <stdint.h>
 #include <stddef.h>
-#include "console.hpp"
+#include <cstdarg>
 
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "font.hpp"
-
+#include "console.hpp"
 
 void* operator new(size_t size, void* buf) {
   return buf;
@@ -17,6 +17,22 @@ void operator delete(void* obj) noexcept {
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
+
+char console_buf[sizeof(Console)];
+Console* console;
+
+int printk(const char* format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config){
   switch (frame_buffer_config.pixel_format) {
@@ -34,13 +50,11 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config){
     }
   }
 
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
-  
-  char buf[128];
-  for (int i = 0; i < 27; ++i) {
-    sprintf(buf, "line %d\n", i);
-    console.PutString(buf);
+  console = new(console_buf) Console(*pixel_writer, {0, 0, 0}, {255, 255, 255});
+
+  for (int i = 0; i < 27; ++i){
+    printk("printk: %d\n", i);
   }
-  
+
   while (1) __asm__("hlt");
 }
