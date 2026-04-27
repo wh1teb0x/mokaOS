e3ee0ae52e9e6a2ea0553f11f6a935f0c36f0bc9
Aki Takeuchi <akinori.take@gmail.com>
2026-04-28 01:38:42 +0900

    add mouse cursor drawing

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -18,6 +18,35 @@ void operator delete(void* obj) noexcept {
 const PixelColor kDesktopBGColor{45, 118, 237};
 const PixelColor kDesktopFGColor{255, 255, 255};
 
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+  "@              ",
+  "@@             ",
+  "@.@            ",
+  "@..@           ",
+  "@...@          ",
+  "@....@         ",
+  "@.....@        ",
+  "@......@       ",
+  "@.......@      ",
+  "@........@     ",
+  "@.........@    ",
+  "@..........@   ",
+  "@...........@  ",
+  "@............@ ",
+  "@......@@@@@@@@",
+  "@......@       ",
+  "@....@@.@      ",
+  "@...@ @.@      ",
+  "@..@   @.@     ",
+  "@.@    @.@     ",
+  "@@      @.@    ",
+  "@       @.@    ",
+  "         @.@   ",
+  "         @@@   ",
+};
+
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
@@ -58,6 +87,16 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config){
 
   console = new(console_buf) Console{*pixel_writer, kDesktopFGColor, kDesktopBGColor};
   printk("Welcome to MokaOS!\n");
+
+  for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
+    for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
+      if (mouse_cursor_shape[dy][dx] == '@') {
+        pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
+      } else if (mouse_cursor_shape[dy][dx] == '.') {
+        pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+      }
+    }
+  }
 
   while (1) __asm__("hlt");
 }
