0a856ceff1024c8b38d3c08f916f7eaa15532ebe
Aki Takeuchi <akinori.take@gmail.com>
2026-05-06 18:09:37 +0900

    add libc++ and newlib syscall stubs for hosted code

--- kernel/newlib_support.c
+++ kernel/newlib_support.c
@@ -2,6 +2,19 @@
 #include <sys/types.h>
 
 caddr_t sbrk(int incr) {
-    errno = ENOMEM; // Error: NO MEMory
-    return (caddr_t)-1;
+  errno = ENOMEM;  // Error: NO MEMory
+  return (caddr_t)-1;
+}
+
+void _exit(void) {
+  while (1) {
+    __asm__("hlt");
+  }
+}
+
+int getpid() { return 1; }
+
+int kill(int pid, int sig) {
+  errno = EINVAL;
+  return -1;
 }
