1b93cc22e0ca8e8196d6fc1f6aefc2e3d98e8a57
Aki Takeuchi <akinori.take@gmail.com>
2026-05-07 15:45:28 +0900

    wire xHCI controller and HID mouse driver into KernelMain

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -10,6 +10,11 @@
 #include "pci.hpp"
 #include "mouse.hpp"
 #include "logger.hpp"
+#include "usb/memory.hpp"
+#include "usb/device.hpp"
+#include "usb/classdriver/mouse.hpp"
+#include "usb/xhci/xhci.hpp"
+#include "usb/xhci/trb.hpp"
 
 void operator delete(void* obj) noexcept {}
 
@@ -26,6 +31,9 @@ PixelWriter* pixel_writer;
 char console_buf[sizeof(Console)];
 Console* console;
 
+char mouse_cursor_buf[sizeof(MouseCursor)];
+MouseCursor* mouse_cursor;
+
 int printk(const char* format, ...) {
   va_list ap;
   int result;
@@ -37,6 +45,33 @@ int printk(const char* format, ...) {
 
   console->PutString(s);
   return result;
+}
+
+void SwitchEhci2Xhci(const pci::Device& xhc_dev) {
+  bool intel_ehc_exist = false;
+  for (int i = 0; i < pci::num_device; ++i) {
+    if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x20u)) {
+      if (0x8086 == pci::ReadVendorId(pci::devices[i])) {
+        intel_ehc_exist = true;
+        break;
+      }
+    }
+  }
+  if (!intel_ehc_exist) {
+    Log(kInfo, "Intel EHCI controller not found. No need to switch.\n");
+    return;
+  }
+
+  uint32_t superspeed_ports = pci::ReadConfReg(xhc_dev, 0xdc);  // USB3PRM
+  pci::WriteConfReg(xhc_dev, 0xd8, superspeed_ports);           // USB3_PSSEN
+  uint32_t ehci2xhci_ports = pci::ReadConfReg(xhc_dev, 0xd4);   // XUSB2PRM
+  pci::WriteConfReg(xhc_dev, 0xd0, ehci2xhci_ports);            // XUSB2PR
+  Log(kDebug, "SwitchEhci2Xhci: SS = %02x, xHCI = %02x\n", superspeed_ports,
+      ehci2xhci_ports);
+}
+
+void MouseObserver(int8_t displacement_x, int8_t displacement_y) {
+  mouse_cursor->MoveRelative({displacement_x, displacement_y});
 }
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
@@ -67,7 +102,62 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       Console{*pixel_writer, kDesktopFGColor, kDesktopBGColor};
   printk("Welcome to MokaOS!\n");
 
+  mouse_cursor = new (mouse_cursor_buf)
+      MouseCursor{pixel_writer, kDesktopBGColor, {300, 200}};
+
   SetLogLevel(kWarn);
 
+  auto err = pci::ScanAllBus();
+  Log(kDebug, "ScanAllBus(): %s\n", err.Name());
+
+  pci::Device* xhc_dev = nullptr;
+  for (int i = 0; i < pci::num_device; ++i) {
+    if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x30u)) {
+      xhc_dev = &pci::devices[i];
+
+      if (0x8086 == pci::ReadVendorId(*xhc_dev)) {
+        break;
+      }
+    }
+  }
+
+  if (xhc_dev) {
+    Log(kInfo, "xHC has been found: %d.%d.%d\n", xhc_dev->bus, xhc_dev->device,
+        xhc_dev->function);
+    const WithError<uint64_t> xhc_bar = pci::ReadBar(*xhc_dev, 0);
+    Log(kDebug, "ReadBar: %s\n", xhc_bar.error.Name());
+    const uint64_t xhc_mmio_base = xhc_bar.value & ~static_cast<uint64_t>(0xf);
+    Log(kDebug, "xHC mmio_base = %08lx\n", xhc_mmio_base);
+
+    usb::xhci::Controller xhc{xhc_mmio_base};
+    if (0x8086 == pci::ReadVendorId(*xhc_dev)) {
+      SwitchEhci2Xhci(*xhc_dev);
+    }
+    {
+      auto err = xhc.Initialize();
+      Log(kDebug, "xHC Initialize: %s\n", err.Name());
+    }
+    Log(kInfo, "xHC starting...\n");
+    xhc.Run();
+    usb::HIDMouseDriver::default_observer = MouseObserver;
+
+    for (int i = 1; i <= xhc.MaxPorts(); ++i) {
+      auto port = xhc.PortAt(i);
+      Log(kDebug, "Port %d: IsConnected=%d\n", i, port.IsConnected());
+      if (port.IsConnected()) {
+        if (auto err = ConfigurePort(xhc, port)) {
+          Log(kError, "Failed to configure port %s: at %s:%d\n", err.Name(),
+              err.File(), err.Line());
+          continue;
+        }
+      }
+    }
+    while (1) {
+      if (auto err = ProcessEvent(xhc)) {
+        Log(kError, "Error while ProcessEvent: %s at %s:%d\n", err.Name(),
+            err.File(), err.Line());
+      }
+    };
+  }
   while (1) __asm__("hlt");
 }
